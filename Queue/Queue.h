bc4f169725d6d8350d8e250b401233c48b50e8ce
Elviro Rocca <elviro.rocca@facile.it>
2015-06-16 14:26:30 +0200

    Initial Commit

---
+++ Queue/Queue.h
@@ -0,0 +1,19 @@
+//
+//  Queue.h
+//  Queue
+//
+//  Created by Elviro Rocca on 16/06/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Queue.
+FOUNDATION_EXPORT double QueueVersionNumber;
+
+//! Project version string for Queue.
+FOUNDATION_EXPORT const unsigned char QueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Queue/PublicHeader.h>
+
+
